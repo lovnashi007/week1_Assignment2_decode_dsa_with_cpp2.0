0c7900d4a98f39eaebd3133a88fa1ba707c26973
lovnashi007 <132254501+lovnashi007@users.noreply.github.com>
2023-08-18 20:28:39 +0530

    Update question_01.cpp

--- question_01.cpp
+++ question_01.cpp
@@ -9,7 +9,7 @@ int main()
     int y, m;
     cout << "Enter second number and value for taking modulus\n";
     cin >> y >> m;       // user will give ‘y’ and 'm' value.
-    int Z = (x * y) % m; // '*' and '%' have same precedence so precendence will decide by moving right to left,so firstly multiply x and y and then modulo operator will resolve the problem so Z = (2*3)%6=6%6=0
+    int Z = (x * y) % m; // '*' and '%' have same precedence so precendence will decide by moving left to right,so firstly multiply x and y and then modulo operator will resolve the problem so Z = (2*3)%6=6%6=0
 
     cout << "Output is: " << Z; // here Z = 0
     return 0;
