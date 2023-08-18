f4f74e14183780a22f53bf06d0944412695324a3
lovnashi007 <132254501+lovnashi007@users.noreply.github.com>
2023-08-18 20:27:31 +0530

    Update question_06.cpp

--- question_06.cpp
+++ question_06.cpp
@@ -4,6 +4,6 @@
 using namespace std;
 int main()
 {
-    int i = (4 + 7 / 5 * 6 * 6 + 9) % 100; // here firstly parenthesis solved then other in parenthesis firstly '/' and '*' will be solved both have same precidence then they are being solved right to left then '+' after that '%' will be solved. then outpu will be i=49%100=49;
+    int i = (4 + 7 / 5 * 6 * 6 + 9) % 100; // here firstly parenthesis solved then other in parenthesis firstly '/' and '*' will be solved both have same precidence then they are being solved left to right then '+' after that '%' will be solved. then outpu will be i=49%100=49;
     cout << i; // 49 will be print.
 }
